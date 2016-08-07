1450dccc59611bbff1ec9263107ef71dbb410e65
kenny lau <kennyllau9@gmail.com>
2016-08-07 01:34:39 -0700

    templating

---
+++ backend/config/routes.js
@@ -0,0 +1,6 @@
+var homeController = require('./../controllers/home.js')
+
+module.exports = function(app){
+
+
+}
---
+++ backend/controllers/home.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var homeDb = mongoose.model('Homedb');
+
+module.exports = (function() {
+	return {
+
+
+	}
+})();
