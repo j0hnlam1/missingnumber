ebdb8a71bbc816ad6213c69342b07d87071c7606
kenny lau <kennyllau9@gmail.com>
2016-08-16 21:19:53 -0700

    Worked on Searchbar

--- frontend/controllers/mapController.js
+++ frontend/controllers/mapController.js
@@ -328,90 +328,82 @@ myApp.controller('mapController', function($scope, $routeParams, NgMap, mapFacto
 
 
 /************SEARCH BAR FOR POKEMON**********/
-    function mapController ($timeout, $q, $log) {
-    var self = this;
-    self.simulateQuery = false;
-    self.isDisabled    = false;
-    self.repos         = loadAll();
-    self.querySearch   = querySearch;
-    self.selectedItemChange = selectedItemChange;
-    self.searchTextChange   = searchTextChange;
+// function DemoCtrl ($timeout, $q, $log) {
+//     var self = this;
+
+    // $scope.simulateQuery = false;
+    // $scope.isDisabled    = false;
+
+//     // list of `state` value/display objects
+    // $scope.states        = loadAll();
+    // console.log($scope.states);
+    // $scope.querySearch   = querySearch;
+    // $scope.selectedItemChange = selectedItemChange;
+    // $scope.searchTextChange   = searchTextChange;
+
+//     self.newState = newState;
+
     // ******************************
     // Internal methods
     // ******************************
+
     /**
-     * Search for repos... use $timeout to simulate
+     * Search for states... use $timeout to simulate
      * remote dataservice call.
      */
-    function querySearch (query) {
-      var results = query ? self.repos.filter( createFilterFor(query) ) : self.repos,
-          deferred;
-      if (self.simulateQuery) {
-        deferred = $q.defer();
-        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-        return deferred.promise;
-      } else {
-        return results;
-      }
-    }
-    function searchTextChange(text) {
-      $log.info('Text changed to ' + text);
-    }
-    function selectedItemChange(item) {
-      $log.info('Item changed to ' + JSON.stringify(item));
-    }
+    // function querySearch (query) {
+    //   var results = query ? $scope.states.filter( createFilterFor(query) ) : $scope.states,
+    //       deferred;
+    //   if ($scope.simulateQuery) {
+    //     deferred = $q.defer();
+    //     $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+    //     return deferred.promise;
+    //   } else {
+    //     return results;
+    //   }
+    // }
+
+    // function searchTextChange(text) {
+    //   $log.info('Text changed to ' + text);
+    // }
+
+    // function selectedItemChange(item) {
+    //   $log.info('Item changed to ' + JSON.stringify(item));
+    // }
+
     /**
-     * Build `components` list of key/value pairs
+     * Build `states` list of key/value pairs
      */
-    function loadAll() {
-      var repos = [
-        {
-          'name'      : 'Angular 1',
-          'url'       : 'https://github.com/angular/angular.js',
-          'watchers'  : '3,623',
-          'forks'     : '16,175',
-        },
-        {
-          'name'      : 'Angular 2',
-          'url'       : 'https://github.com/angular/angular',
-          'watchers'  : '469',
-          'forks'     : '760',
-        },
-        {
-          'name'      : 'Angular Material',
-          'url'       : 'https://github.com/angular/material',
-          'watchers'  : '727',
-          'forks'     : '1,241',
-        },
-        {
-          'name'      : 'Bower Material',
-          'url'       : 'https://github.com/angular/bower-material',
-          'watchers'  : '42',
-          'forks'     : '84',
-        },
-        {
-          'name'      : 'Material Start',
-          'url'       : 'https://github.com/angular/material-start',
-          'watchers'  : '81',
-          'forks'     : '303',
-        }
-      ];
-      return repos.map( function (repo) {
-        repo.value = repo.name.toLowerCase();
-        return repo;
-      });
-    }
+    // function loadAll() {
+    //   var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+    //           Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+    //           Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+    //           Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+    //           North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+    //           South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+    //           Wisconsin, Wyoming';
+
+    //   return allStates.split(/, +/g).map( function (state) {
+    //     return {
+    //       value: state.toLowerCase(),
+    //       display: state
+    //     };
+    //   });
+    // }
+
     /**
      * Create filter function for a query string
      */
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-      return function filterFn(item) {
-        return (item.value.indexOf(lowercaseQuery) === 0);
-      };
-    }
-  }
+    // function createFilterFor(query) {
+    //   var lowercaseQuery = angular.lowercase(query);
 
-  /***************** END OF SERACH BAR *******************/
+    //   return function filterFn(state) {
+    //     return (state.value.indexOf(lowercaseQuery) === 0);
+    //   };
+
+    // }
+  // }
+
+/***************** END OF SERACH BAR *******************/
 
 });
