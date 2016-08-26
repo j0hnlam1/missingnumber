dbe36c2d3db08715f26d2ca4be027dab8b6e5ff3
kenny lau <kennyllau9@gmail.com>
2016-08-26 16:06:26 -0700

    commented out message not working

--- frontend/controllers/chatController.js
+++ frontend/controllers/chatController.js
@@ -15,64 +15,64 @@
             userSocket = data;
         })
 
-        if ($scope.person == undefined) {
-            userFactory.getUser(function(data){
-                var x = $.isEmptyObject(data);
-                if (x == true) {
-                    console.log("no user logged in");
-                } else {
-                    socket.emit("login", data[0].name);
-                    $scope.person = data;
-                }
-            });
-        }
+        // if ($scope.person == undefined) {
+        //     userFactory.getUser(function(data){
+        //         var x = $.isEmptyObject(data);
+        //         if (x == true) {
+        //             console.log("no user logged in");
+        //         } else {
+        //             socket.emit("login", data[0].name);
+        //             $scope.person = data;
+        //         }
+        //     });
+        // }
     
-        socket.on('messages', function(messages){
-            $scope.messages = messages;
-            $scope.$digest();
-        });
+        // socket.on('messages', function(messages){
+        //     $scope.messages = messages;
+        //     $scope.$digest();
+        // });
 
         socket.on("userlist", function(userlist){
             $scope.userlist = userlist;
             $scope.$digest();
             console.log($scope.userlist);
         });
-        if ($scope.person == undefined) {
-            socket.on("data", function(data) {
-                var res = data.info.slice(2);
-                if (res == userSocket.id) {
-                    var x = $.isEmptyObject(data.data);
-                    if (x == true) {
-                        console.log("no user logged in");
-                    } else {
-                        socket.emit("login", data.data[0].name);
-                        $scope.person = data.data;
-                    }
-                    socket.removeListener('data');
+        // if ($scope.person == undefined) {
+        //     socket.on("data", function(data) {
+        //         var res = data.info.slice(2);
+        //         if (res == userSocket.id) {
+        //             var x = $.isEmptyObject(data.data);
+        //             if (x == true) {
+        //                 console.log("no user logged in");
+        //             } else {
+        //                 socket.emit("login", data.data[0].name);
+        //                 $scope.person = data.data;
+        //             }
+        //             socket.removeListener('data');
 
-                }
-            })
-        }
+        //         }
+        //     })
+        // }
 
             // socket.on("users", function(data){
             //     $scope.allUsers = data;
             //     console.log($scope.allUsers);
             // });
 
-        $scope.sendMessage = function(message){
-            message.name = $scope.person[0].name;
-            socket.emit('new_message', message);
-            document.getElementById('message').value = "";
-        }
-        document.getElementById("message").addEventListener("keydown", function(e) {
-            if (!e) { 
-                var e = window.event;
-            }
-            // Enter is pressed
-            if (e.keyCode == 13) {
-                $scope.sendMessage($scope.newMessage);
-            }
-        }, false);
+        // $scope.sendMessage = function(message){
+        //     message.name = $scope.person[0].name;
+        //     socket.emit('new_message', message);
+        //     document.getElementById('message').value = "";
+        // }
+        // document.getElementById("message").addEventListener("keydown", function(e) {
+        //     if (!e) { 
+        //         var e = window.event;
+        //     }
+        //     // Enter is pressed
+        //     if (e.keyCode == 13) {
+        //         $scope.sendMessage($scope.newMessage);
+        //     }
+        // }, false);
 
 		
     } // ends chatController function
